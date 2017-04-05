2b7911642c703999ebeebd3d1e3bf339c0ef545b
Stephen Scott <s.p.scott@kainos.com>
2017-04-05 21:59:44 +0100

    Option to add favicon

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ var explorerHtml = '<form id="api_selector">'+
                         '<div class="input"><a id="explore" class="header__btn" href="#" data-sw-translate>Explore</a></div>' +
                       '</form>';
 
-var setup = function(swaggerDoc, explorer, options, customCss) {
+var favIconHtml = '<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />' +
+                  '<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />'
+
+
+var setup = function(swaggerDoc, explorer, options, customCss, customfavIcon) {
 	options = options || {};
     customCss = customCss || '';
+    customfavIcon = customfavIcon || false;
 	var html = fs.readFileSync(__dirname + '/indexTemplate.html');
     try {
     	fs.unlinkSync(__dirname + '/index.html');
@@ -20,11 +25,13 @@ var setup = function(swaggerDoc, explorer, options, customCss) {
     }
     var htmlWithSwaggerReplaced = html.toString().replace('<% swaggerDoc %>', JSON.stringify(swaggerDoc));
     var explorerString = explorer ?  explorerHtml : '';
+    var favIconString = customfavIcon ? '<link rel="icon" href="' + customfavIcon + '" />' : favIconHtml;
     var explorerHtmlWithSwagger = htmlWithSwaggerReplaced.replace('<% explorerString %>', explorerString);
     var indexHTML = explorerHtmlWithSwagger.replace('<% customOptions %>', JSON.stringify(options))
-    var htmlWithCustomCss  = indexHTML.replace('<% customCss %>', customCss); 
+    var htmlWithCustomCss  = indexHTML.replace('<% customCss %>', customCss);
+    var htmlWithFavIcon  = htmlWithCustomCss.replace('<% favIconString %>', favIconString);
     
-    return function(req, res) { res.send(htmlWithCustomCss) };
+    return function(req, res) { res.send(htmlWithFavIcon) };
 };
 
 var serve = express.static(__dirname + '/static')
--- test/testapp/app.js
+++ test/testapp/app.js
@@ -3,6 +3,14 @@ var app = express();
 var swaggerUi = require('../../index');
 var swaggerDocument = require('./swagger.json');
 
+app.use((req, res, next) => {
+	if (req.url === '/favicon.ico') {
+		res.sendFile(__dirname + '/favicon.ico');
+	} else {
+		next();
+	}
+});
+
 var options = {
 	validatorUrl : null,
 	oauth: {
@@ -15,7 +23,7 @@ var options = {
  }
 };
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, false, options));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, false, options, ''));
 
 app.get('/', function(req, res) { res.json({ status: 'OK'}); });
 app.get('/bar', function(req, res) { res.json({ status: 'OKISH'}); });
