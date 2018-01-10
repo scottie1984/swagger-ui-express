6b15bc8f43bc4597ad95575ca31c48ead8432c6e
Amir Sasson <amirsas@gmail.com>
2018-01-10 10:46:47 +0200

    add customJs option

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ var fs = require('fs');
 var express = require('express');
 
 var favIconHtml = '<link rel="icon" type="image/png" href="./favicon-32x32.png" sizes="32x32" />' +
-                  '<link rel="icon" type="image/png" href="./favicon-16x16.png" sizes="16x16" />'
+  '<link rel="icon" type="image/png" href="./favicon-16x16.png" sizes="16x16" />'
 
 
-var setup = function(swaggerDoc, opts, options, customCss, customfavIcon, swaggerUrl, customeSiteTitle) {
+var setup = function (swaggerDoc, opts, options, customCss, customfavIcon, swaggerUrl, customeSiteTitle) {
   var isExplorer
   if (opts && typeof opts === 'object') {
     isExplorer = opts.explorer
     options = opts.swaggerOptions
     customCss = opts.customCss
+    customJs = opts.customJs
     customfavIcon = opts.customfavIcon
     swaggerUrl = opts.swaggerUrl
     customeSiteTitle = opts.customSiteTitle
@@ -20,51 +21,52 @@ var setup = function(swaggerDoc, opts, options, customCss, customfavIcon, swagge
     //support legacy params based function
     isExplorer = opts
   }
-	options = options || {};
-  var explorerString = isExplorer ?  '' : '.swagger-ui .topbar .download-url-wrapper { display: none }';
-    customCss = explorerString + ' ' + customCss || explorerString;
-    customfavIcon = customfavIcon || false;
-    customeSiteTitle = customeSiteTitle || 'Swagger UI';
-	var html = fs.readFileSync(__dirname + '/indexTemplate.html');
-    try {
-    	fs.unlinkSync(__dirname + '/index.html');
-    } catch (e) {
+  options = options || {};
+  var explorerString = isExplorer ? '' : '.swagger-ui .topbar .download-url-wrapper { display: none }';
+  customCss = explorerString + ' ' + customCss || explorerString;
+  customfavIcon = customfavIcon || false;
+  customeSiteTitle = customeSiteTitle || 'Swagger UI';
+  var html = fs.readFileSync(__dirname + '/indexTemplate.html');
+  try {
+    fs.unlinkSync(__dirname + '/index.html');
+  } catch (e) {
 
-    }
+  }
 
-    var favIconString = customfavIcon ? '<link rel="icon" href="' + customfavIcon + '" />' : favIconHtml;
-    var htmlWithCustomCss  = html.toString().replace('<% customCss %>', customCss);
-    var htmlWithFavIcon  = htmlWithCustomCss.replace('<% favIconString %>', favIconString);
+  var favIconString = customfavIcon ? '<link rel="icon" href="' + customfavIcon + '" />' : favIconHtml;
+  var htmlWithCustomCss = html.toString().replace('<% customCss %>', customCss);
+  var htmlWithFavIcon = htmlWithCustomCss.replace('<% favIconString %>', favIconString);
+  var htmlWithCustomJs = htmlWithFavIcon.replace('<% customJs %>', options.customJs || '');
 
-    var initOptions = {
-      swaggerDoc: swaggerDoc || undefined,
-      customOptions: options,
-      swaggerUrl: swaggerUrl || undefined
-    }
-    var htmlWithOptions = htmlWithFavIcon.replace('<% swaggerOptions %>', JSON.stringify(initOptions)).replace('<% title %>', customeSiteTitle)
+  var initOptions = {
+    swaggerDoc: swaggerDoc || undefined,
+    customOptions: options,
+    swaggerUrl: swaggerUrl || undefined
+  }
+  var htmlWithOptions = htmlWithCustomJs.replace('<% swaggerOptions %>', JSON.stringify(initOptions)).replace('<% title %>', customeSiteTitle)
 
-    return function(req, res) { res.send(htmlWithOptions) };
+  return function (req, res) { res.send(htmlWithOptions) };
 };
 
 var serve = express.static(__dirname + '/static');
 
-var stringify = function(obj, prop) {
+var stringify = function (obj, prop) {
   var placeholder = '____FUNCTIONPLACEHOLDER____';
   var fns = [];
-  var json = JSON.stringify(obj, function(key, value) {
+  var json = JSON.stringify(obj, function (key, value) {
     if (typeof value === 'function') {
       fns.push(value);
       return placeholder;
     }
     return value;
   }, 2);
-  json = json.replace(new RegExp('"' + placeholder + '"', 'g'), function(_) {
+  json = json.replace(new RegExp('"' + placeholder + '"', 'g'), function (_) {
     return fns.shift();
   });
   return json + ';';
 };
 
 module.exports = {
-	setup: setup,
-	serve: serve
+  setup: setup,
+  serve: serve
 };
