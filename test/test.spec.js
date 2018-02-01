67bc81f7bcb22844d6eb57f61f886712ccb38dec
Aaron Ambrose <aaronambrose@Aarons-MacBook-Pro.local>
2018-01-31 16:44:25 -0800

    updated tests to use non express based version of library

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,42 +1,43 @@
-var assert = require('assert');
-var app = require('./testapp/app');
-var http = require('http');
-var phantom = require('phantom');
-require('es6-shim');
+var assert = require("assert");
+var app = require("./testapp/app");
+var http = require("http");
+var phantom = require("phantom");
+require("es6-shim");
 
-describe('integration', function() {
+describe("integration", function() {
   var server;
   var sitepage = null;
-	var phInstance = null;
+  var phInstance = null;
 
   before(function(done) {
-		app.set('port', 3001);
+    app.set("port", 3001);
 
-		server = app.listen(app.get('port'), function() {
-			done();
-		});
-	});
+    server = app.listen(app.get("port"), function() {
+      done();
+    });
+  });
 
   after(function() {
-		server.close();
-		sitepage.close();
-		phInstance.exit();
-	});
+    server.close();
+    sitepage.close();
+    phInstance.exit();
+  });
 
-	it('should have API Documentation hosted at /api-docs', function(done) {
+  it("should have API Documentation hosted at /api-docs", function(done) {
     this.timeout(30000);
-    phantom.create()
+    phantom
+      .create()
       .then(function(instance) {
         phInstance = instance;
         return instance.createPage();
       })
       .then(function(page) {
         sitepage = page;
-        return page.open('http://localhost:3001/api-docs/');
+        return page.open("http://localhost:3001/api-docs/");
       })
       .then(function(status) {
         setTimeout(function() {
-          assert.equal('success', status);
+          assert.equal("success", status);
           done();
         }, 100);
       })
@@ -45,44 +46,13 @@ describe('integration', function() {
       });
   });
 
-  it('should contain the expected elements on the page', function(done) {
-    sitepage.property('title')
+  it("should contain the expected elements on the page", function(done) {
+    sitepage
+      .property("title")
       .then(function(title) {
-        assert.equal('Swagger UI', title);
+        assert.equal("Swagger UI", title);
         return sitepage.evaluate(function() {
-          return document.querySelector('.swagger-ui').innerHTML;
-        });
-      })
-      .then(function(html) {
-        assert.ok(html);
-        assert.notEqual(html.indexOf('id="operations-/test-index"'), -1);
-        assert.notEqual(html.indexOf('id="operations-/test-impossible"'), -1);
-        done();
-      })
-      .catch(function(err) {
-        done(err);
-      });
-	});
-
-  it('should have API Documentation hosted at /api-docs-from-url', function(done) {
-    sitepage.open('http://localhost:3001/api-docs-from-url/')
-      .then(function(status) {
-        setTimeout(function() {
-          assert.equal('success', status);
-          done();
-        }, 100);
-      })
-      .catch(function(err) {
-        done(err);
-      });
-  });
-
-  it('should contain the expected elements on the page', function(done) {
-    sitepage.property('title')
-      .then(function(title) {
-        assert.equal('Swagger UI', title);
-        return sitepage.evaluate(function() {
-          return document.querySelector('.swagger-ui').innerHTML;
+          return document.querySelector(".swagger-ui").innerHTML;
         });
       })
       .then(function(html) {
@@ -96,11 +66,12 @@ describe('integration', function() {
       });
   });
 
-  it('should have API Documentation hosted at /api-docs-using-object', function(done) {
-    sitepage.open('http://localhost:3001/api-docs-using-object/')
+  it("should have API Documentation hosted at /api-docs-from-url", function(done) {
+    sitepage
+      .open("http://localhost:3001/api-docs-from-url/")
       .then(function(status) {
         setTimeout(function() {
-          assert.equal('success', status);
+          assert.equal("success", status);
           done();
         }, 100);
       })
@@ -109,12 +80,13 @@ describe('integration', function() {
       });
   });
 
-  it('should contain the expected elements on the page for api-docs-using-object', function(done) {
-    sitepage.property('title')
+  it("should contain the expected elements on the page", function(done) {
+    sitepage
+      .property("title")
       .then(function(title) {
-        assert.equal('Swagger UI', title);
+        assert.equal("Swagger UI", title);
         return sitepage.evaluate(function() {
-          return document.querySelector('.swagger-ui').innerHTML;
+          return document.querySelector(".swagger-ui").innerHTML;
         });
       })
       .then(function(html) {
@@ -128,11 +100,12 @@ describe('integration', function() {
       });
   });
 
-  it('should have API Documentation hosted at /api-docs-with-null', function(done) {
-    sitepage.open('http://localhost:3001/api-docs-with-null/')
+  it("should have API Documentation hosted at /api-docs-using-object", function(done) {
+    sitepage
+      .open("http://localhost:3001/api-docs-using-object/")
       .then(function(status) {
         setTimeout(function() {
-          assert.equal('success', status);
+          assert.equal("success", status);
           done();
         }, 100);
       })
@@ -141,12 +114,47 @@ describe('integration', function() {
       });
   });
 
-  it('should contain the expected elements on the page for api-docs-with-null', function(done) {
-    sitepage.property('title')
+  it("should contain the expected elements on the page for api-docs-using-object", function(done) {
+    sitepage
+      .property("title")
       .then(function(title) {
-        assert.equal('Swagger UI', title);
+        assert.equal("Swagger UI", title);
         return sitepage.evaluate(function() {
-          return document.querySelector('.swagger-ui').innerHTML;
+          return document.querySelector(".swagger-ui").innerHTML;
+        });
+      })
+      .then(function(html) {
+        assert.ok(html);
+        assert.notEqual(html.indexOf('id="operations-/test-index"'), -1);
+        assert.notEqual(html.indexOf('id="operations-/test-impossible"'), -1);
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
+
+  it("should have API Documentation hosted at /api-docs-with-null", function(done) {
+    sitepage
+      .open("http://localhost:3001/api-docs-with-null/")
+      .then(function(status) {
+        setTimeout(function() {
+          assert.equal("success", status);
+          done();
+        }, 100);
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
+
+  it("should contain the expected elements on the page for api-docs-with-null", function(done) {
+    sitepage
+      .property("title")
+      .then(function(title) {
+        assert.equal("Swagger UI", title);
+        return sitepage.evaluate(function() {
+          return document.querySelector(".swagger-ui").innerHTML;
         });
       })
       .then(function(html) {
